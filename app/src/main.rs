a388d61216ebe56fc02b8a8480aa7bc6a11abb11
Yuto Nishida <yuto@berkeley.edu>
2024-11-08 19:28:15 -0800

    Init code

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,21 @@
+mod cli {
+    use clap::Parser;
+    use std::path::PathBuf;
+
+    #[derive(Parser, Debug)]
+    #[command(version, about, long_about = None)]
+
+    pub struct Args {
+        /// The directory where the repositories are stored
+        pub repos_directory: Option<PathBuf>,
+    }
+
+    pub fn get_args() -> Args {
+        Args::parse()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = cli::get_args();
+
 }
